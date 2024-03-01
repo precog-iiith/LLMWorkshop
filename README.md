a0565931bdec735725029c17ea589ae64d11ed20
Prashant Kodali <kodali.prashant@gmail.com>
2024-03-01 17:36:58 +0530

    session3 - added langchain and streamlit apps ; reorg of repo - moved session3 to session3 folder

--- README.md
+++ README.md
@@ -6,3 +6,14 @@
 - We have created a bunch of assignments for you to try things and submit.
 - Your feedback will help us improve material presented during the workshop. Fill in [this Form (takes ~4 mins)](https://forms.office.com/r/2xPT7D7P0G)
 - Once you are done with the assignments, create a github repo with all the necessary files, make the repo public and fill in [this form (takes ~4 mins](https://forms.office.com/r/puwCBbFq5V). 
+
+
+
+
+# Session 3 - Using LLMs - Open-Source and Commercial APIs
+- [Commercial APIs](./Session3/commercial_llm_apis.ipynb)
+- [Open-Source LLMs for Inference on GPUs](./Session3/) (Run on GoogleColab or similar services)
+- [Open-Source LLMs fine-tuning on GPUs](./Session3/fine-tuning-llms.ipynb) (Run on GoogleColab or similar services)
+- [Open-Source LLMs on Local Machines (Laptop/PCs) using Ollama](./Session3/ollama.md)
+- [LLM Apps using LangChain and Ollama](./Session3/langchainpy_with_ollama.md)
+- [LLM Apps with Frontend using LangChain + Ollama + Streamlit](./Session3/LLMAppWIthFrontEnd)
---
+++ Session3/LLMAppWIthFrontEnd/rag.py
@@ -0,0 +1,59 @@
+from langchain.vectorstores import Chroma
+from langchain.chat_models import ChatOllama
+from langchain.embeddings import FastEmbedEmbeddings
+from langchain.schema.output_parser import StrOutputParser
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.prompts import PromptTemplate
+from langchain.vectorstores.utils import filter_complex_metadata
+
+
+class ChatPDF:
+    vector_store = None
+    retriever = None
+    chain = None
+
+    def __init__(self):
+        self.model = ChatOllama(model="phi")
+        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=100)
+        self.prompt = PromptTemplate.from_template(
+            """
+            <s> [INST] You are an assistant for question-answering tasks. Use the following pieces of retrieved context 
+            to answer the question. If you don't know the answer, just say that you don't know. Use three sentences
+             maximum and keep the answer concise. [/INST] </s> 
+            [INST] Question: {question} 
+            Context: {context} 
+            Answer: [/INST]
+            """
+        )
+
+    def ingest(self, pdf_file_path: str):
+        docs = PyPDFLoader(file_path=pdf_file_path).load()
+        chunks = self.text_splitter.split_documents(docs)
+        chunks = filter_complex_metadata(chunks)
+
+        vector_store = Chroma.from_documents(documents=chunks, embedding=FastEmbedEmbeddings())
+        self.retriever = vector_store.as_retriever(
+            search_type="similarity_score_threshold",
+            search_kwargs={
+                "k": 3,
+                "score_threshold": 0.5,
+            },
+        )
+
+        self.chain = ({"context": self.retriever, "question": RunnablePassthrough()}
+                      | self.prompt
+                      | self.model
+                      | StrOutputParser())
+
+    def ask(self, query: str):
+        if not self.chain:
+            return "Please, add a PDF document first."
+
+        return self.chain.invoke(query)
+
+    def clear(self):
+        self.vector_store = None
+        self.retriever = None
+        self.chain = None
