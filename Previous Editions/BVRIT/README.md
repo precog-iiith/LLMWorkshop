3d9ac1db22936a38a1fae6dba0dc4984d8efc2b2
ritwikmishra <ritwik.m07@gmail.com>
2024-03-09 16:38:38 +0530

    ihub presentation

---
+++ Previous Editions/BVRIT/README.md
@@ -0,0 +1,21 @@
+<h1 align="center">PreCog LLM Workshop</h1>
+<p align="center"><i>Code, Documentation to follow along with the workshop. </i></p>
+
+- Run the notebooks that need GPU (high compute) on Google Colab/Kaggle Notebooks.
+- Create new issue if you need help with anything (even prior or after the workshop). A sample issue is created.
+- We have created a bunch of assignments for you to try things and submit - and will be introducing them along the workshop.
+- Your feedback will help us improve material presented during the workshop. Fill in [this Form (takes ~4 mins)](https://forms.office.com/r/2xPT7D7P0G)
+- Once you are done with the assignments, create a github repo with all the necessary files, make the repo public and fill in [this form (takes ~4 mins](https://forms.office.com/r/puwCBbFq5V). 
+
+
+# Session 1 - LLM Capabilities(./Session1)
+
+# [Session 2 - LLMs in depth](./Session2) 
+
+# Session 3 - Using LLMs - Open-Source and Commercial APIs
+- [Commercial APIs](./Session3/commercial_llm_apis.ipynb)
+- [Open-Source LLMs for Inference on GPUs](./Session3/generation_with_opensource_LLMs.ipynb) (Run on GoogleColab or similar services)
+- [Open-Source LLMs fine-tuning on GPUs](./Session3/fine-tuning-llms.ipynb) (Run on GoogleColab or similar services)
+- [Open-Source LLMs on Local Machines (Laptop/PCs) using Ollama](./Session3/ollama.md)
+- [LLM Apps using LangChain and Ollama](./Session3/langchainpy_with_ollama.md)
+- [LLM Apps with Frontend using LangChain + Ollama + Streamlit](./Session3/LLMAppWIthFrontEnd)
---
+++ Previous Editions/BVRIT/Session3/LLMAppWIthFrontEnd/app.py
@@ -0,0 +1,66 @@
+import os
+import tempfile
+import streamlit as st
+from streamlit_chat import message
+from rag import ChatPDF
+
+st.set_page_config(page_title="ChatPDF")
+
+
+def display_messages():
+    st.subheader("Chat")
+    for i, (msg, is_user) in enumerate(st.session_state["messages"]):
+        message(msg, is_user=is_user, key=str(i))
+    st.session_state["thinking_spinner"] = st.empty()
+
+
+def process_input():
+    if st.session_state["user_input"] and len(st.session_state["user_input"].strip()) > 0:
+        user_text = st.session_state["user_input"].strip()
+        with st.session_state["thinking_spinner"], st.spinner(f"Thinking"):
+            agent_text = st.session_state["assistant"].ask(user_text)
+
+        st.session_state["messages"].append((user_text, True))
+        st.session_state["messages"].append((agent_text, False))
+
+
+def read_and_save_file():
+    st.session_state["assistant"].clear()
+    st.session_state["messages"] = []
+    st.session_state["user_input"] = ""
+
+    for file in st.session_state["file_uploader"]:
+        with tempfile.NamedTemporaryFile(delete=False) as tf:
+            tf.write(file.getbuffer())
+            file_path = tf.name
+
+        with st.session_state["ingestion_spinner"], st.spinner(f"Ingesting {file.name}"):
+            st.session_state["assistant"].ingest(file_path)
+        os.remove(file_path)
+
+
+def page():
+    if len(st.session_state) == 0:
+        st.session_state["messages"] = []
+        st.session_state["assistant"] = ChatPDF()
+
+    st.header("ChatPDF")
+
+    st.subheader("Upload a document")
+    st.file_uploader(
+        "Upload document",
+        type=["pdf"],
+        key="file_uploader",
+        on_change=read_and_save_file,
+        label_visibility="collapsed",
+        accept_multiple_files=True,
+    )
+
+    st.session_state["ingestion_spinner"] = st.empty()
+
+    display_messages()
+    st.text_input("Message", key="user_input", on_change=process_input)
+
+
+if __name__ == "__main__":
+    page()
